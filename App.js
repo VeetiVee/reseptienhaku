9799b9fe6d94ec93065d6f49c0a05d2220915f33
VeetiVee <veeti.vainikka@gmail.com>
2022-02-14 11:31:14 +0200

    lisäsin tehdyn tehtävän

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Alert, Image } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [apis, setApis] = useState([]);
+
+  const getApis = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
+    .then(response => response.json())
+    .then(data => setApis(data.meals))
+    .catch(error => {
+      Alert.alert('Error', error); 
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        placeholder='keyword'
+        onChangeText={ text => setKeyword(text)}
+        />
+      <Button onPress={getApis} title="Find"/>
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) =>
+      <View>
+        <Text style={{fontSize:18, fontWeight: "bold"}}>{item.strMeal}</Text>
+        <Image source={{uri: item.strMealThumb }} style={{height:100, width:100}}/>
+      </View>}
+        data={apis} /> 
       <StatusBar style="auto" />
     </View>
   );
